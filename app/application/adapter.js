c846100b837ea887799aca6787ec8372297356c6
Chelsea Sanders <cksanders1@gmail.com>
2015-12-22 08:23:46 +0000

    BM-306: Create RESTAdapter that will be used by the entire application

---
+++ app/application/adapter.js
@@ -0,0 +1,11 @@
+/**
+ * @file RESTAdapter implementation.
+ * Ember CLI Mirage 0.1.x exposes a REST API and Ember Data should be aware. Sole purpose is to define the path on the
+ * server where we can find our RESTful service.
+ */
+import DS from 'ember-data';
+
+export default DS. JSONAPIAdapter.extend({
+  host: 'http://localhost:8080',
+  namespace: 'api'
+});

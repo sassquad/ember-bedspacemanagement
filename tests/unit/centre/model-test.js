3962c8e125ff3b400ee8b8b0ab007b683a94599d
Stephen Scott <sa_scott@yahoo.com>
2015-12-22 17:03:38 +0000

    Add model to needs construct
    The unit test for the model can need the centre model.

--- tests/unit/centre/model-test.js
+++ tests/unit/centre/model-test.js
@@ -2,7 +2,9 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('centre', 'Unit | Model | centre', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    model: centre
+  ]
 });
 
 test('it exists', function(assert) {

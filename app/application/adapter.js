793178052eeba5a9ca77b25dabfbff20c1545150
Chelsea Sanders <cksanders1@gmail.com>
2015-12-22 09:06:56 +0000

    DEPRECATION: The default behavior of shouldReloadAll will change in Ember Data 2.0 to always return false when there is at least one "centre" record in the store.

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -7,5 +7,8 @@ import DS from 'ember-data';
 
 export default DS. JSONAPIAdapter.extend({
   host: 'http://localhost:8080',
-  namespace: 'api'
+  namespace: 'api',
+  shouldReloadAll() {
+    return true;
+  }
 });

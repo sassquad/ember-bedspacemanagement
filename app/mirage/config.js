edeb269848e69fe48598f26a3c03881baff56f6d
Chelsea Sanders <cksanders1@gmail.com>
2015-12-22 08:31:06 +0000

    BM-306: Update mirage config

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,82 +1,53 @@
-export default function() {
+/**
+ * @file Specify API end-points.
+ * Mirage wraps Pretender, which intercepts requests that would normally hit your API, and allows you to specify
+ * the response that should be sent back.
+ */
+export default function () {
+  /**
+   * As Sails.js API is on a different server
+   */
+  this.urlPrefix = 'http://localhost:8080';
 
-  // These comments are here to help you get started. Feel free to delete them.
+  /**
+   * API is namespaced
+   */
+  this.namespace = 'api';
 
-  /*
-    Config (with defaults).
+  /**
+   * Timing parameter of the response.
+   *  Default is a 400ms delay during development and 0 delay in testing (so your tests run fast).
+   */
+  this.timing = 400;
 
-    Note: these only affect routes defined *after* them!
-  */
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  /**
+   * Setup endpoints for retrieving fake models
+   *  Route is made dynamic by responding with all the centres in Mirage's in-memory database.
+   */
 
-  /*
-    Route shorthand cheatsheet
-  */
-  /*
-    GET shorthands
+  /**
+   * Route will handle the URL '/api/centres'
+   */
+  this.get('/centres', function (db) {
+    return {
+      data: db.centres.map(attrs => (
+      {type: 'centres', id: attrs.id, attributes: attrs}
+      ))
+    };
+  });
 
-    // Collections
-    this.get('/contacts');
-    this.get('/contacts', 'users');
-    this.get('/contacts', ['contacts', 'addresses']);
+  /**
+   * Route will handle the URL '/api/centre/{id}'
+   */
+  this.get('/centres/:id', function (db, request) {
+    let id = request.params.id;
 
-    // Single objects
-    this.get('/contacts/:id');
-    this.get('/contacts/:id', 'user');
-    this.get('/contacts/:id', ['contact', 'addresses']);
-  */
-
-  /*
-    POST shorthands
-
-    this.post('/contacts');
-    this.post('/contacts', 'user'); // specify the type of resource to be created
-  */
-
-  /*
-    PUT shorthands
-
-    this.put('/contacts/:id');
-    this.put('/contacts/:id', 'user'); // specify the type of resource to be updated
-  */
-
-  /*
-    DELETE shorthands
-
-    this.del('/contacts/:id');
-    this.del('/contacts/:id', 'user'); // specify the type of resource to be deleted
-
-    // Single object + related resources. Make sure parent resource is first.
-    this.del('/contacts/:id', ['contact', 'addresses']);
-  */
-
-  /*
-    Function fallback. Manipulate data in the db via
-
-      - db.{collection}
-      - db.{collection}.find(id)
-      - db.{collection}.where(query)
-      - db.{collection}.update(target, attrs)
-      - db.{collection}.remove(target)
-
-    // Example: return a single object with related models
-    this.get('/contacts/:id', function(db, request) {
-      var contactId = +request.params.id;
-
-      return {
-        contact: db.contacts.find(contactId),
-        addresses: db.addresses.where({contact_id: contactId})
-      };
-    });
-
-  */
+    return {
+      data: {
+        type: 'centres',
+        id: id,
+        attributes: db.centres.find(id)
+      }
+    };
+  });
 }
-
-/*
-You can optionally export a config that is only loaded during tests
-export function testConfig() {
-
-}
-*/
